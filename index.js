7823aca9289303b6e715234b08b583fd0544f155
Rachel-prog-design <rachelnyinawabana7@gmail.com>
2026-02-17 15:06:46 +0200

    arrays

---
+++ index.js
@@ -0,0 +1,14 @@
+// Exercise 1: array initialization and property Access
+const productCategories = ["Electronics", "Clothing", "Books", "Home & Kitchen", "Books", "Toys & Games"];
+
+//a
+console.log(productCategories[0]); 
+//b
+console.log(productCategories[productCategories.length - 1]);
+//c
+console.log(productCategories.length);
+//d
+productCategories[1] = "pen";
+
+//e
+productCategories
